5c1e667eed727ec4bc0de9eec6b92c6650943a2f
jauresmanouan <jauresmanouan@gmail.com>
2024-07-08 10:39:43 +0000

    ✨ T01 : Create routing system for all pages, create component <Erro404/> and delete style in App.css and index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import AboutUs from "./components/pages/aboutUS/AboutUs.jsx";
+import Activities from "./components/pages/activities/Activities.jsx";
+import Home from "./components/pages/home/Home.jsx";
+import JoinUs from "./components/pages/joinUs/JoinUs.jsx";
+import Error404 from "./components/pages/error/Error404.jsx"
 
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/nos-activités" element={<Activities />} />
+      <Route path="/nous-connaître" element={<AboutUs />} />
+      <Route path="/nous-rejoindre" element={<JoinUs />} />
+      <Route path="*" element={<Error404 />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/components/pages/joinUs/JoinUs.jsx
@@ -0,0 +1,5 @@
+export default function JoinUs() {
+  return (
+    <div>JoinUs</div>
+  )
+}
