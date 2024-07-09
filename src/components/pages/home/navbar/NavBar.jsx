a66a9cb4e4c4e9b86d890bf4c77f117dca8ac3ee
jauresmanouan <jauresmanouan@gmail.com>
2024-07-09 01:22:57 +0000

    ✨ T01 : NavBar > Add button rejoindre with arrow and style

--- src/components/pages/home/navbar/NavBar.jsx
+++ src/components/pages/home/navbar/NavBar.jsx
@@ -1,14 +1,21 @@
+import styled from "styled-components";
+import { GoArrowRight } from "react-icons/go";
+
 export default function NavBar() {
   return (
-    <div className="h-24 flex justify-between items-center bg-fushia font-inter font-semibold text-lg	">
+    <NavBarStyled className="h-24 flex justify-between items-center bg-fushia font-inter font-semibold text-lg	">
       <div className="logo ml-40 text-white">Logo</div>
-      <div className="links mr-40 list-none flex gap-8 text-white">
+      <div className="links mr-40 list-none flex items-center  gap-8 text-white">
         <li>Accueil</li>
         <li>Nos activités</li>
         <li>Nous connaître</li>
-        <li>Nous rejoindre</li>
+        <li className="rejoindre flex justify-center items-center gap-1 w-48 h-14 bg-white rounded-full">
+          <button className="text-fushia">Nous rejoindre</button>
+          <GoArrowRight className="text-fushia w-5 h-5" />
+        </li>
       </div>
-    </div>
+    </NavBarStyled>
   );
 }
 
+const NavBarStyled = styled.div``;
