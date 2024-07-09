2b46d851d07f5dc20bc7d97f3e34514162650321
jauresmanouan <jauresmanouan@gmail.com>
2024-07-09 00:36:26 +0000

    📦 T01 : NavBar > Install styled component and refacto tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,29 +3,15 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors : {
-        fushia : "#F30E3F",
-        blue: "#09FE3",
-        turquoise : "65C1C0",
-        white: "FFFFFF",
-        black: "000000"
+      colors: {
+        fushia: "#F30E3F",
+        blue: "#009FE3",
+        turquoise: "#65C1C0",
       },
       fontFamily: {
-        montserrat :['Inter', 'sans-serif'],
-        inter:['Montserrat', 'serif']
+        inter: ["Inter", "sans-serif"],
+        montserrat: ["Montserrat", "serif"],
       },
-      fontWeight: {
-        thin: 100,
-        extralight: 200,
-        light: 300,
-        normal: 400,
-        medium: 500,
-        semibold: 600,
-        bold: 700,
-        extrabold: 800,
-        black: 900,
-      }
-
     },
   },
   plugins: [],
