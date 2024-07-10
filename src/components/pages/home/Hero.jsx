7e88db74c2049f99376da4e2e5158751ae456547
jauresmanouan <jauresmanouan@gmail.com>
2024-07-10 10:58:18 +0000

    ✨ T02 : HERO > Left side of hero style done and right side prepare for logo also start make it responsive but need to refacto and correct some bug

--- src/components/pages/home/Hero.jsx
+++ src/components/pages/home/Hero.jsx
@@ -2,13 +2,13 @@ import Button from "../../reusable-ui/Button";
 
 export default function Hero() {
   return (
-    <div className="grid grid-cols-2 grid-rows-1 items-center justify-center h-[calc(100vh-6rem)] px-10 lg:px-40">
-      <div className="space-y-5">
-        <h1 className=" text-6xl font-montserrat font-extrabold leading-tight">
+    <div className="md:grid grid-cols-2 grid-rows-1 items-center justify-center h-[calc(100vh-6rem)] px-10 lg:px-40">
+      <div className="border-red-700 space-y-5 flex flex-col justify-center items-center md:items-start text-center md:text-left m-8 md:m-0">
+        <h1 className="text-3xl md:text-6xl font-montserrat font-extrabold leading-tight">
           Inspirez la nation, <br />
           construisez l’avenir
         </h1>
-        <h2 className="text-blue font-inter font-bold">
+        <h2 className="text-blue text-sm md:text-lg font-inter font-bold">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate,
           hic. Commodi minima dolores natus sequi voluptates cupiditate unde
         </h2>
@@ -19,7 +19,7 @@ export default function Hero() {
           arrow={false}
         />
       </div>
-      <div className="border-2 border-green-600 ">b</div>
+      <img src="" alt="logo" className="w-full h-96 bg-red-600" />
     </div>
   );
 }
