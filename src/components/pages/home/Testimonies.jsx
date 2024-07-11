5fc1cbba7c46653fcc0937fd3d3210603906f9ae
jauresmanouan <jauresmanouan@gmail.com>
2024-07-11 22:50:43 +0000

    🚧 T05 : TESTIMONIES > Done with layout and style we need to refacto and make it responsive

--- src/components/pages/home/Testimonies.jsx
+++ src/components/pages/home/Testimonies.jsx
@@ -16,29 +16,38 @@ export default function Testimonies() {
             <HiOutlineChevronRight className="text-white" />
           </button>
         </div>
-        <div className="grid grid-cols-2 grid-rows-1 card lg:card-side rounded-none bg-base-100 shadow-xl h-[420px] w-[1080px]">
-          <figure>
-            <img
-              className="h-[420px] w-[588px] object-cover"
-              src="src/assets/images/Aletier IFL 240623 4.png"
-              alt="Album"
-            />
-          </figure>
-          <div className="flex flex-col justify-center p-8">
-            <h2 className="text-blue text-4xl font-inter font-black mb-5">
-              “Transformation...”
-            </h2>
-            <p className="font-inter font-normal mb-8">
-              Lorem ipsum dolor sit amet consectetur adipiscing elit ultrices
-              scelerisque mi sed interdum lacus tellus in mi orc, netus nisl
-              laoreet phasellus. Pellentesque non nunc placerat mi quis vitae
-              cursus ornare.
-            </p>
-            <p className="text-turquoise font-inter font-semibold mb-2">
-              Bénédicte KOFFI
-            </p>
-            <p className="font-inter font-medium">Ingénieur Zootechnique</p>
-          </div>
+        <div className="flex flex-col space-y-4 justify-center items-center">
+            <div className="grid grid-cols-2 grid-rows-1 card lg:card-side rounded-none bg-base-100 shadow-xl h-[420px] w-[1080px]">
+              <figure>
+                <img
+                  className="h-[420px] w-[588px] object-cover"
+                  src="src/assets/images/Aletier IFL 240623 4.png"
+                  alt="Album"
+                />
+              </figure>
+              <div className="flex flex-col justify-center p-8">
+                <h2 className="text-blue text-4xl font-inter font-black mb-5">
+                  “Transformation...”
+                </h2>
+                <p className="font-inter font-normal mb-8">
+                  Lorem ipsum dolor sit amet consectetur adipiscing elit ultrices
+                  scelerisque mi sed interdum lacus tellus in mi orc, netus nisl
+                  laoreet phasellus. Pellentesque non nunc placerat mi quis vitae
+                  cursus ornare.
+                </p>
+                <p className="text-turquoise font-inter font-semibold mb-2">
+                  Bénédicte KOFFI
+                </p>
+                <p className="font-inter font-medium">Ingénieur Zootechnique</p>
+              </div>
+            </div>
+            <div className="flex space-x-2 ">
+              <div className="w-4 h-4 rounded-full bg-white"></div>
+              <div className="w-4 h-4 rounded-full bg-white"></div>
+              <div className="w-4 h-4 rounded-full bg-white"></div>
+              <div className="w-4 h-4 rounded-full bg-fushia"></div>
+              <div className="w-4 h-4 rounded-full bg-white"></div>
+            </div>
         </div>
       </div>
     </div>
