c5bf0ef6834c3027e72cc2089c0d7fc1d9cd9388
jauresmanouan <jauresmanouan@gmail.com>
2024-07-18 01:03:26 +0000

    📱 T07 : CONTENT > End with responsive layout

--- src/components/pages/home/Content.jsx
+++ src/components/pages/home/Content.jsx
@@ -2,17 +2,22 @@ import React from "react";
 
 export default function Content() {
   return (
-    <div className="flex bg-green-500 h-[46rem] w-full">
-      <div className="bg-blue h-full w-full">Image</div>
-      <div className="flex flex-col">
-        <p className="bg-white h-[387px] w-[46rem] font-inter font-bold text-4xl leading-tight pr-60 pl-16 pb-10 flex justify-end items-end">
-          Lorem ipsum dolor sit amet consectetur adipiscing elidolor sit nullam
-          neques.
-        </p>
-        <p className="bg-turquoise h-[387px] w-full font-inter font-bold text-4xl leading-tight pr-60 pl-16 pt-10">
-          Lorem ipsum dolor sit amet iquam sit nullam neques.
-        </p>
+    <div className="grid grid-cols-1 grid-rows-4 h-screen sm:grid-cols-2 sm:grid-rows-2 ">
+      <div className="bg-blue row-span-2 order-4 sm:order-none relative">
+        <img
+          src=""
+          alt="lucette"
+          className="absolute origin-bottom bottom-0 left-0 sm:left-24 scale-50 sm:scale-75"
+        />
       </div>
+      <p className="bg-white">
+        Lorem ipsum dolor sit amet consectetur adipiscing elidolor sit nullam
+        neques.
+      </p>
+      <p className="bg-turquoise">
+        Lorem ipsum dolor sit amet iquam sit nullam neques.
+      </p>
     </div>
   );
 }
+
