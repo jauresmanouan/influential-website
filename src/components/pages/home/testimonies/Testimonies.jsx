fb7c7f4ee0f9a9f49a25a6ebb6760e0070b58abb
jauresmanouan <jauresmanouan@gmail.com>
2024-07-31 03:33:56 +0000

    🐛 T08 : Fix bug on position for button

--- src/components/pages/home/testimonies/Testimonies.jsx
+++ src/components/pages/home/testimonies/Testimonies.jsx
@@ -1,10 +1,9 @@
-import React from "react";
 import TestimoniesSlider from "./TestimoniesSlider";
 import ButtonPrevNext from "./ButtonPrevNext";
 
 export default function Testimonies() {
   return (
-    <div className="h-[50rem] w-screen bg-blue flex flex-col justify-center content-start">
+    <div className="h-[50rem] w-screen bg-blue flex flex-col justify-center content-start relative">
       <h1 className="font-montserrat text-white font-extrabold text-4xl sm:text-6xl md:text-7xl mb-5 md:mb-14 lg:ml-48">
         Que disent ils ? 🤔
       </h1>
