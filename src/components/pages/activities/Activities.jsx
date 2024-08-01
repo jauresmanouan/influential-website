e46712f74419214d08d2ed0331bdaa9f26fb9192
jauresmanouan <jauresmanouan@gmail.com>
2024-08-01 01:31:53 +0000

    📱 T09 : CARDS > Create <CardsActivities/> and make it responsive

--- src/components/pages/activities/Activities.jsx
+++ src/components/pages/activities/Activities.jsx
@@ -1,20 +1,13 @@
 import CallToAction from "../home/CallToAction";
 import Footer from "../home/Footer";
-import Cards from "../home/News/Cards";
 import NavBar from "../home/navbar/NavBar";
-import { activities } from "./activities";
+import CardsActivties from "./CardsActivties";
 
 export default function Activities() {
   return (
     <div>
       <NavBar />
-      <div className="h-[90rem] flex flex-col justify-center items-center">
-        <h1 className="font-montserrat font-extrabold text-center text-4xl sm:text-7xl">
-          Nous menons 5 <br />
-          différentes <span className="text-fushia">activités</span> 🤩
-        </h1>
-        <Cards array={activities} />
-      </div>
+      <CardsActivties />
       <CallToAction text={"Restez informés de nos activités"} button={false} />
       <Footer />
     </div>
