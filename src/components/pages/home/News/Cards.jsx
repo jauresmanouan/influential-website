a0b9fa9eca4f218d507b4037e1d6899f73711845
jauresmanouan <jauresmanouan@gmail.com>
2024-08-01 02:22:02 +0000

    🐛 T09 : CARDS > Fix overlaping when open menu in mobile version

--- src/components/pages/home/News/Cards.jsx
+++ src/components/pages/home/News/Cards.jsx
@@ -2,7 +2,7 @@ import Card from "../Card";
 
 export default function Cards({ array, title }) {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center relative -z-10">
       <div className="xl:h-full p-20 max-lg:p-2 flex justify-center items-start flex-col space-y-10">
         <h1 className="font-montserrat font-extrabold text-4xl sm:text-7xl">
           {title}
