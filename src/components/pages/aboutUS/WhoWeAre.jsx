1ce84a896fc91c4664a98c95fb709ef87f2880ce
jauresmanouan <jauresmanouan@gmail.com>
2024-08-04 10:59:01 +0000

    📱 T11 : TEAM > Responsive is okay for WhoWeAre

--- src/components/pages/aboutUS/WhoWeAre.jsx
+++ src/components/pages/aboutUS/WhoWeAre.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 export default function WhoWeAre() {
   return (
-    <div className="h-full ml-40 mr-40 mt-20 mb-20">
+    <div className="h-full mx-4 sm:mx-10 md:mx-20 lg:mx-40 my-20">
       <h1 className="font-montserrat font-extrabold text-3xl sm:text-5xl lg:text-7xl leading-tight mb-10">
         Qui sommes <span className="text-fushia">nous</span> ? 🤷🏾‍♂️
       </h1>
-      <p className="font-inter font-s text-2xl text-justify leading-normal rounded-3xl">
+      <p className="font-inter font-s text-base sm:text-lg md:text-xl lg:text-2xl text-justify leading-normal rounded-3xl">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus
         quam eveniet voluptatum eos molestias. Dolores expedita soluta iste enim
         labore alias placeat eaque. Illum laudantium ipsa illo iusto deleniti
-        officiis. <br/>
+        officiis. <br />
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus
         quam eveniet voluptatum eos molestias. Dolores expedita soluta iste enim
         labore alias placeat eaque. Illum laudantium ipsa illo iusto deleniti
