9e333a0fdce3a85d4ded06ac684dbf2542157420
jauresmanouan <jauresmanouan@gmail.com>
2024-08-23 18:21:34 +0000

    ✨ 💄 T12 : Buil component <BlurredImage/> for blurring image

---
+++ src/components/reusable-ui/BlurredImage.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { BlurhashCanvas } from "react-blurhash";
+
+export default function BlurredImage({ src, alt, hash, className }) {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = src;
+    img.onload = () => {
+      setImageLoaded(true);
+    };
+  }, [src]);
+
+  return (
+    <>
+      <div style={{ display: imageLoaded ? "none" : "inline" }}>
+        <BlurhashCanvas
+          hash={hash}
+          className={className}
+          punch={1}
+          resolutionX={32}
+          resolutionY={32}
+        />
+      </div>
+      <img
+        src={src}
+        alt={alt}
+        loading="auto"
+        style={{ display: !imageLoaded ? "none" : "inline" }}
+        className={className}
+      />
+    </>
+  );
+}
